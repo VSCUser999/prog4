956f51e6c024739609bb751c3e8f979c57dec9ed
VSCUser999 <def8663@gmail.com>
2021-10-15 09:55:35 +0800

    HM20211015_0955

--- prog4.c
+++ prog4.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 int main (void){
-    int iNn1,iNn2,iNn3,iCount1,iCount2;
-    scanf("%d",&iNn1);
-    iNn3 = iNn1-1;
-    iNn2 = iNn3;
-    for(iCount1 = 0; iCount1 < iNn1; iCount1++){
-        for(iCount2 = 0; iCount2 < ((iNn1-1) * 2 + 1); iCount2++){
-            if(iCount2 <= iNn3 && iCount2 >= iNn2){
-                printf("*");              
-            }else {
-                printf(" ");
-            }  
-        }
-        iNn2--;
-        iNn3++;
-        printf("\n");
-   }
+    int iCount1;
+    float fMax,fMin,fTemp;
+    
+    fMax = -99999.0;
+    fMin = 99999.0;
+    for(iCount1 = 0;iCount1 < 10;iCount1++){
+        scanf("%f",&fTemp);
+        if (fTemp > fMax) fMax = fTemp;
+        if (fTemp < fMin) fMin = fTemp;
+    }
+    printf("max:%4.2f\n",fMax);
+    printf("min:%4.2f\n",fMin);
 }
