d38926541bb18a2d1673f3f704204675fe89ceea
VSCUser999 <def8663@gmail.com>
2021-10-15 10:31:12 +0800

    HM20211015_1031

--- prog4.c
+++ prog4.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <float.h>
+
 int main (void){
     int iCount1;
     float fMax,fMin,fTemp;
 
-    fMax = -9999999999.0;
-    fMin =  9999999999.0;
+    fMax = -FLT_MAX;
+    fMin =  FLT_MAX;
     for(iCount1 = 0;iCount1 < 10;iCount1++){
         scanf("%f",&fTemp);
         if (fTemp > fMax) fMax = fTemp;
